1bc35b5f2e1bd83b4e9b3d268a98c143afcc9b0a
Gabriel Volpe <volpegabriel@gmail.com>
2019-09-07 16:28:12 -0500

    Ordering and weakening examples

--- src/Foldy.hs
+++ src/Foldy.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE DataKinds, TemplateHaskell, TypeApplications #-}
+{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses, TypeSynonymInstances #-}
 
 module Foldy where
 
@@ -6,9 +7,18 @@ import           Refined
 
 type TenElems = Refined (SizeEqualTo 10) [Int]
 type FiveToTenElems = Refined (And (SizeGreaterThan 4) (SizeLessThan 11)) [Int]
+type Asc = Refined Ascending [Int]
 
 fixed :: TenElems
 fixed = $$(refineTH [1..10])
 
 range :: FiveToTenElems
 range = $$(refineTH [1..5])
+
+ordering :: Asc
+ordering = $$(refineTH [1,2])
+
+instance Weaken (SizeEqualTo 10) (And (SizeGreaterThan 4) (SizeLessThan 11))
+
+weakening :: FiveToTenElems
+weakening = weaken fixed
