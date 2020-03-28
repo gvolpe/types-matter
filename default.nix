33303290d931b1b8cd5d6287115e7abaeb022054
Gabriel Volpe <volpegabriel@gmail.com>
2020-03-28 12:38:02 +0100

    reducing nix files to a single one

--- default.nix
+++ default.nix
@@ -1,2 +1,21 @@
-{ nixpkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs-channels/archive/1fe82110feb.tar.gz") {}, compiler ? "ghc865" }:
-nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./types-matter.nix { }
+{ nixpkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs-channels/archive/1fe82110feb.tar.gz") {} }:
+
+let
+  inherit (nixpkgs) haskellPackages;
+  drv = haskellPackages.callCabal2nix "types-matter" ./. {};
+
+in
+  {
+    my_project = drv;
+    shell = haskellPackages.shellFor {
+      withHoogle = true;
+      packages = p: [drv];
+      buildInputs = with haskellPackages; [ hlint cabal-install ];
+      shellHook = ''
+        export NIX_GHC="$(which ghc)"
+        export NIX_GHCPKG="$(which ghc-pkg)"
+        export NIX_GHC_DOCDIR="$NIX_GHC/../../share/doc/ghc/html"
+        export NIX_GHC_LIBDIR="$(ghc --print-libdir)"
+      '';
+    };
+  }.shell
