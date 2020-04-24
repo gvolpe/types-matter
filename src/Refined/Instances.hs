05ccac1397518d9b4f277e14c7442034636240e1
Gabriel Volpe <volpegabriel@gmail.com>
2020-04-24 14:48:51 +0200

    Updating nixpkgs, now compiling with GHC-8.8.3 - closes #2

--- src/Refined/Instances.hs
+++ src/Refined/Instances.hs
@@ -8,7 +8,6 @@ import           Data.Text                      ( Text
                                                 , unpack
                                                 )
 import           Data.Typeable                  ( typeOf )
-import           Language.Haskell.TH.Syntax     ( Lift(..) )
 import           Prelude                 hiding ( null )
 import           Refined
 
@@ -16,7 +15,3 @@ instance Predicate NonEmpty Text where
   --validate p value = validate p (unpack value) -- validate using String instance
   validate p value = when (null value)
     $ throwRefineOtherException (typeOf p) "Text cannot be empty"
-
--- Or use: https://hackage.haskell.org/package/th-lift-instances
-instance Lift Text where
-  lift t = lift (unpack t)
